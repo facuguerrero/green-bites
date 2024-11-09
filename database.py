4d32aa1fed0f8f58ad880cc6500eec2822b3626d
Facundo Guerrero <facundoiguerrero@gmail.com>
2024-11-09 15:16:59 +0100

    envvar for db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@db:5432/points_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
